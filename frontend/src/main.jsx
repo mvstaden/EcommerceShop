8948cb93b6888056926f0a39c6ccd10f9cbac1c9
Marius Van Staden <mvstaden91@gmail.com>
2025-01-16 05:51:55 +0200

    Created Home & product Screen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
+import App from "./App.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import ProductPage from "./pages/ProductPage.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index={true} element={<HomePage />} />
+          <Route path="/products/:id" element={<ProductPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import { Col, Row } from "react-bootstrap";
+import products from "../products";
+import ProductCard from "../components/ProductCard";
+const HomePage = () => {
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <ProductCard product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+export default HomePage;
